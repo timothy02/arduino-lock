e3dbd51740fe577b52c0273b5f8f905be95a6ca1
Tymoteusz Cierzan <tymekcierzan@gmail.com>
2019-12-26 22:41:39 +0100

    Code refactor

--- arduino-lock.ino
+++ arduino-lock.ino
@@ -1,7 +1,27 @@
 #include <Keypad.h>
-
 #include <IRremote.h>
 #include <IRremoteInt.h>
+
+/* GLOBAL VARIABLES */
+bool isTypingNewCode = false;
+bool isOpen = false;
+
+const int GREEN_LIGHT = 12;
+const int YELLOW_LIGHT = 11;
+const int RED_LIGHT = 10;
+
+const int STATE_OPEN = 1;
+const int STATE_NEW = 2;
+const int STATE_CLOSED = 0;
+
+const int DEFAULT_CODE_LENGTH = 8;
+char DEFAULT_CODE[DEFAULT_CODE_LENGTH] = {'1', '2', '3', '4', '5', '6', '7', '8'};
+
+
+int appState = STATE_CLOSED;
+char CODE[DEFAULT_CODE_LENGTH];
+char input[DEFAULT_CODE_LENGTH];
+int inputCounter = 0;
 
 /* KEYPAD INIT */
 const byte COLS = 4;
@@ -21,24 +41,7 @@ byte colPins[COLS] = {2, 3, 4, 5};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 
-/* ACCESS CODE */
-boolean isOpen = false;
-boolean isTypingNewCode = false;
-
-const int GREEN_LIGHT = 12;
-const int YELLOW_LIGHT = 11;
-const int RED_LIGHT = 10;
-
-const int STATE_OPEN = 1;
-const int STATE_NEW = 2;
-const int STATE_CLOSED = 0;
-
-char CODE[8];
-char input[8];
-int inputCounter = 0;
-
-
-/* RC CODE */
+/* RC INIT */
 const int RC_PIN = A0;
 IRrecv irrecv(RC_PIN);
 decode_results results;
@@ -48,131 +51,113 @@ void setup() {
   pinMode(YELLOW_LIGHT, OUTPUT);
   pinMode(RED_LIGHT, OUTPUT);
 
-  CODE[0] = '1';
-  CODE[1] = '2';
-  CODE[2] = '3';
-  CODE[3] = '4';
-  CODE[4] = '5';
-  CODE[5] = '6';
-  CODE[6] = '7';
-  CODE[7] = '0';
-
   irrecv.enableIRIn();
   
- Serial.begin(9600);
+  Serial.begin(9600);
+
+  setCode(DEFAULT_CODE);
+  setState(STATE_CLOSED);
 }
 
 void loop() {
+  char key = resolveInput();
+
+  performAction(key);
+  setLights(appState);
+}
+
+char resolveInput(){
   char key = keypad.getKey();
+  if (irrecv.decode(&results)){
+       irrecv.resume();   // Receive the next value
+  }
+  
+  if(decodeIrValue(results.value) != NO_KEY){
+    key = decodeIrValue(results.value);
+  }
 
-   if (irrecv.decode(&results)) 
-    {
-         irrecv.resume();   // Receive the next value
-    }
-    if(results.value != 4294967295 && results.value != 0){
-      Serial.println(results.value);
-    }
+  return key;
+}
 
-    if(results.value == 16738455){
-      key = '0';
-    }
+void performAction(char key){
+  if(appState == STATE_OPEN){
+      switch(key){
+        case 'A':
+          resetInput();
+        break;
+        
+        case 'B':
+          Serial.println("-- TYPING NEW CODE --");
+          setState(STATE_NEW);
+        break;
 
-    if(results.value == 16724175){
-      key = '1';
-    }
+        case 'C':
+          Serial.println("-- LOCKED --");
+          setState(STATE_CLOSED);
+        break;
 
-    if(results.value == 15669479){
-      key = '2';
+        case NO_KEY:
+        break;
+        
+        default:
+          blinkYellow();
+        break;
     }
+  }
 
-    if(results.value == 16743045){
-      key = '3';
-    }
-
-    if(results.value == 16716015){
-      key = '4';
-    }
-
-    if(results.value == 16726215){
-      key = '5';
-    }
-
-    if(results.value == 16734885){
-      key = '6';
-    }
-
-    if(results.value == 16728765){
-      key = '7';
-    }
-
-    if(results.value == 16730805){
-      key = '8';
-    }
-
-    if(results.value == 16732845){
-      key = '9';
-    }
-
-    if(results.value == 16750695){
-      key = 'A';
-    }
-
-    if(results.value == 16756815){
-      key = 'B';
-    }
- 
-
-    if(results.value == 16769055){
-      key = 'C';
-    }
-
-    if(results.value == 16754775){
-      key = 'D';
-    }
- 
-
-  if(key != NO_KEY){
-    if(!isTypingNewCode){
-     blinkYellow();
-    } else {
-      if(readInputForNewCode(key)){ //gets fired when new code is entered
-        isTypingNewCode = false;
+  if(appState == STATE_NEW){
+    switch(key){
+      case 'A':
         resetInput();
-      }
-    }
+      break;
 
-    if(!isTypingNewCode){
-      if(key == 'A'){
-        Serial.println("-- CLEAR INPUT --");
+      case 'B': //Ignore those control keys
+      case 'C':
+      case 'D':
+      case NO_KEY:
+      break;
+
+      default:
+        if(readInputForNewCode(key)){ //gets fired when new code is entered
+          resetInput();
+          setState(STATE_OPEN);
+        }
+      break;
+    }
+  }
+
+  if(appState == STATE_CLOSED){
+    switch(key){
+      case 'A':
         resetInput();
-      } else if(key == 'B'){
-        Serial.println("-- LOCK --");
-        resetInput();
-        isOpen = false;
-      } else if (key == 'C' && isOpen){
-        Serial.println("-- TYPING NEW INPUT --");
-        resetInput();
-        isTypingNewCode = true;
-        setLights(STATE_NEW);
-      } else{
+        blinkYellow();
+      break;
+  
+      case 'B': //Ignore those control keys
+      case 'C':
+      case 'D':
+      case NO_KEY:
+      break;
+  
+      default:
         readInput(key);
-      }
+        blinkYellow();
+      break;
     }
-  }
 
-  if(!isOpen && checkCode()){
-    Serial.println("-- UNLOCK --");
-    resetInput();
-    isOpen = true;
-  }
-
-  if(isOpen && !isTypingNewCode){
-     setLights(STATE_OPEN);
-  } else if(!isTypingNewCode){
-    setLights(STATE_CLOSED);
+    if(checkCode()){
+      setState(STATE_OPEN);
+      Serial.println("-- UNLOCK --");
+      resetInput();
+   }
   }
 }
 
+void setState(int state){
+  appState = state;
+
+  setLights(state);
+}
 
 void setLights(int state){
   switch(state){
@@ -231,6 +216,7 @@ bool readInputForNewCode(char key){
 }
 
 void resetInput(){
+  Serial.println("-- CLEAR INPUT --");
     for(int i=0; i<8; i++){
       input[i] = 0x00;  
     }
@@ -248,4 +234,59 @@ bool checkCode(){
   }
 
   return result;
+}
+
+void setCode(char newCode[]){
+  for(int i=0; i<8; i++){
+    CODE[i] = newCode[i];
+  }
+}
+
+char decodeIrValue(unsigned long irCode){
+  switch(irCode){
+    case 16738455:
+      return '0';
+
+    case 16724175:
+      return '1';
+
+    case 15669479:
+      return '2';
+
+    case 16743045:
+      return '3';
+
+    case 16716015:
+      return '4';
+
+    case 16726215:
+      return '5';
+
+    case 16734885:
+      return '6';
+
+    case 16728765:
+      return '7';
+
+    case 16730805:
+      return '8';
+
+    case 16732845:
+      return '9';
+
+    case 16750695:
+      return 'A';
+
+    case 16756815:
+      return 'B';
+
+    case 16769055:
+      return 'C';
+
+    case 16754775:
+      return 'D';
+
+    default:
+      return NO_KEY;
+  }
 }
